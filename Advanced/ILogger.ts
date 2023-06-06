d7535e868e8fede697de2996d348a7909060e726
Kesarapu <2165117@cognizant.com>
2023-06-06 13:51:54 +0530

    secound commit

--- Advanced/ILogger.ts
+++ Advanced/ILogger.ts
@@ -1,20 +1,19 @@
 import { injectable, inject, Container } from 'inversify';
-
 import "reflect-metadata";
 
 interface ILogger {
-
 log(msg: string): void;
 
 }
 
+//to automatically resolve all the dependencies
 @injectable()
 class consoleLogger implements ILogger {
 log(msg: string): void {
 
 console.log('Logging output in console:', msg);
 
-}
+    }
 }
 
 @injectable()
@@ -24,43 +23,18 @@ log(msg: string): void {
 
  console.log('Logging output in console:', msg);
 
- }
+    }
 
 }
 
-@injectable()
-class loggerContainerService {
-
-private logger: ILogger;
-
-constructor(@inject('ILogger') logger: ILogger) {
-
-this.logger = logger;
-
- }
-
- public printMessage(message: string): void {
-
- this.logger.log(message);
-
- }
-
-}
 
 const container = new Container();
+container.bind<ILogger>('fileLogger').to(fileLogger);
+container.bind<ILogger>('consoleLogger').to(consoleLogger);
 
-const logger = true;
+const object1 = container.get<ILogger>('fileLogger');
+const object2 = container.get<ILogger>('consoleLogger');
 
-if (logger) {
+object1.log("filelogger");
+object2.log("consolelog");
 
- container.bind<ILogger>('ILogger').to(fileLogger);
-
-} else {
-
- container.bind<ILogger>('ILogger').to(consoleLogger);
-
-}
-
-const object = container.resolve<loggerContainerService>(loggerContainerService);
-
-object.printMessage('output');
